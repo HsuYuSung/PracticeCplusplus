0dd8660c004e677a654258b9f534d8b01edea7c4
ishtien <203isthebest@gmail.com>
2024-10-22 22:51:52 +0800

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -3,6 +3,7 @@ using namespace std;
 int main(){
     cout << "Hello world!world world world" << endl;
     cout << "Hello world!world world" << endl;
+    cout << "Hello world!world world" << endl;
     cout << "Hello world! Hi Hi" << endl;
     cout << "Hello world! Hi Hi" << endl;
 }
